255c0767574d6750dcadacc3ac068fda76c383cc
JaneHerrick <poporona@rambler.ru>
2023-06-15 00:09:10 +0300

    Initial commit

---
+++ SB_CPP_PR_17.5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Vector
+{
+private:
+	double x;
+	double y;
+	double z;
+public:
+	Vector() : x(0), y(0), z(0) {};
+	Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {};
+	void showVec()
+	{
+		cout << x << " " << y <<" " << z << endl;
+	}
+	int showVecModule()
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+
+};
+
+
+void main()
+{
+	Vector a(4, 4, 4);
+	a.showVec();
+
+	cout << endl;
+	cout << "Module of vector = " << a.showVecModule() << endl;
+}
